9782989082521810828c5a8f33c72c312572e732
Anas Al Saieed <alsaieedanas@gmail.com>
2024-05-07 23:27:53 +0300

    fetching

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,39 @@ import { useEffect } from "react";
 function App() {
   useEffect(function () {
     async function predict() {
-      const res = await fetch("http://machine-api-eq7w.onrender.com/predict", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          gender: "Male",
-          age: 20.0,
-          hypertension: 1,
-          heart_disease: 0,
-          ever_married: "Yes",
-          work_type: "children",
-          Residence_type: "Rural",
-          avg_glucose_level: 100.0,
-          bmi: 90.0,
-          smoking_status: "never smoked",
-          smoking_not_found: "False",
-        }),
-      });
-      const data = await res.json();
-      console.log(data);
+      try {
+        const res = await fetch(
+          "https://machine-api-eq7w.onrender.com/predict",
+          {
+            method: "POST",
+
+            headers: { "Content-Type": "application/json" },
+
+            body: JSON.stringify({
+              gender: "Male",
+              age: 20.0,
+              hypertension: 1,
+              heart_disease: 0,
+              ever_married: "Yes",
+              work_type: "children",
+              Residence_type: "Rural",
+              avg_glucose_level: 100.0,
+              bmi: 90.0,
+              smoking_status: "never smoked",
+              smoking_not_found: "False",
+            }),
+          }
+        );
+        const data = await res.json();
+        console.log(data);
+      } catch (err) {
+        console.log(err.message);
+      } finally {
+        console.log("done");
+      }
     }
     predict();
-  });
+  }, []);
   return <h1 className=" text-red-400">hello react</h1>;
 }
 
