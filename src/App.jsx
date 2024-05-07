2092b9e8a411f5408f0d3763c1e74e0024693972
Anas Al Saieed <alsaieedanas@gmail.com>
2024-05-07 22:59:49 +0300

    data fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
+import { useEffect } from "react";
+
 function App() {
-  return <h1>hello react</h1>;
+  useEffect(function () {
+    async function predict() {
+      const res = await fetch("http://machine-api-eq7w.onrender.com/predict", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          gender: "Male",
+          age: 20.0,
+          hypertension: 1,
+          heart_disease: 0,
+          ever_married: "Yes",
+          work_type: "children",
+          Residence_type: "Rural",
+          avg_glucose_level: 100.0,
+          bmi: 90.0,
+          smoking_status: "never smoked",
+          smoking_not_found: "False",
+        }),
+      });
+      const data = await res.json();
+      console.log(data);
+    }
+    predict();
+  });
+  return <h1 className=" text-red-400">hello react</h1>;
 }
 
 export default App;
