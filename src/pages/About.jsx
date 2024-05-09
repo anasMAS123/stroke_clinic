fc7b89fa30456b3f7d6cf04c3e77e7a5d8c603ec
Anas Al Saieed <alsaieedanas@gmail.com>
2024-05-09 19:32:01 +0300

    small update 1

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,7 @@ import Button from "../ui/Button";
 
 function About() {
   return (
-    <div className="bg-[#3C5B6F] flex flex-col space-y-4 justify-center items-center text-white">
+    <div className="bg-[#3C5B6F] flex flex-col space-y-4 justify-center items-center text-white text-center sm:text-start">
       <p className="text-3xl font-semibold italic">
         the web application helps you to determine if you are at a risk of
         stroke by using <span className=" text-green-500">AI model</span>.
